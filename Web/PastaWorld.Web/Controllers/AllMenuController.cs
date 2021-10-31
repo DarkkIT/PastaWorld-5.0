de7cf285b8bf94f15bb997e960c288a05d93d5dc
DarkkIT <50321264+DarkkIT@users.noreply.github.com>
2021-10-31 20:03:19 +0200

    Add category menu page

---
+++ Web/PastaWorld.Web/Controllers/AllMenuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PastaWorld.Web.Controllers
+{
+    public class AllMenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
