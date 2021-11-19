e41b66b53a1a12c0bd23ac6d521ab2b43b9282ba
DarkkIT <50321264+DarkkIT@users.noreply.github.com>
2021-11-20 01:13:33 +0200

    Add inmenu buttons

---
+++ Web/PastaWorld.Web/Controllers/InformationController.cs
@@ -0,0 +1,17 @@
+﻿namespace PastaWorld.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
