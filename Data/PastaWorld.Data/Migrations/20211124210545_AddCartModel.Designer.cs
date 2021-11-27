f54be266feae4eac54774277001032e1e5be38ac
DarkkIT <50321264+DarkkIT@users.noreply.github.com>
2021-11-27 23:23:45 +0200

    Change menu logos

--- Data/PastaWorld.Data/Migrations/20211124210545_AddCartModel.Designer.cs
+++ Data/PastaWorld.Data/Migrations/20211124210545_AddCartModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PastaWorld.Data;
 
 namespace PastaWorld.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211124210545_AddCartModel")]
+    partial class AddCartModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
