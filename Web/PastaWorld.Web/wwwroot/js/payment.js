ea2c0af2d934533c6c6e2cfaaadc9131d252b77a
EmilVuchev <emo_vuchev_90@abv.bg>
2022-03-27 14:00:40 +0300

    Refactor payment view and javascript

--- Web/PastaWorld.Web/wwwroot/js/payment.js
+++ Web/PastaWorld.Web/wwwroot/js/payment.js
@@ -1,41 +1,78 @@
-﻿let anchorGuest = document.getElementById('guest');
+﻿// Payment form login/client info 
+let anchorGuest = document.getElementById('guest');
 let anchorClient = document.getElementById('client');
 
 let guestElements = document.getElementsByClassName('guest');
 let loginElements = document.getElementsByClassName('login');
 
-for (var i = 0; i < loginElements.length; i++) {
-    loginElements[i].style['display'] = 'none';
-}
+let displayNone = 'none';
+let displayAll = '';
 
-anchorGuest.style['color'] = "rgb(232 232 232)";
+setDisplayProperty(loginElements, displayNone);
+let blackColor = "rgb(25 25 25)";
+let blueColor = "rgb(3 102 214)";
+let bold = 'bold';
+let normal = 'normal';
+
+anchorGuest.style['color'] = blackColor;
+anchorGuest.style['fontWeight'] = bold;
 
 anchorGuest.addEventListener('click',
     function (event) {
         event.preventDefault();
-        anchorGuest.style['color'] = "rgb(232 232 232)";
-        anchorClient.style['color'] = "rgb(3 102 214)";
+        anchorGuest.style['color'] = blackColor;
+        anchorClient.style['color'] = blueColor;
 
-        for (var i = 0; i < guestElements.length; i++) {
-            guestElements[i].style['display'] = '';
-        }
+        anchorGuest.style['fontWeight'] = bold;
+        anchorClient.style['fontWeight'] = normal;
 
-        for (var i = 0; i < loginElements.length; i++) {
-            loginElements[i].style['display'] = 'none';
-        }
+        setDisplayProperty(guestElements, displayAll);
+        setDisplayProperty(loginElements, displayNone);
     });
 
 anchorClient.addEventListener('click',
     function (event) {
         event.preventDefault();
-        anchorClient.style['color'] = "rgb(232 232 232)";
-        anchorGuest.style['color'] = "rgb(3 102 214)";
+        anchorClient.style['color'] = blackColor;
+        anchorGuest.style['color'] = blueColor;
 
-        for (var i = 0; i < guestElements.length; i++) {
-            guestElements[i].style['display'] = 'none';
-        }
+        anchorClient.style['fontWeight'] = bold;
+        anchorGuest.style['fontWeight'] = normal;
 
-        for (var i = 0; i < loginElements.length; i++) {
-            loginElements[i].style['display'] = '';
+        setDisplayProperty(guestElements, displayNone);
+        setDisplayProperty(loginElements, displayAll);
+    }
+);
+
+function setDisplayProperty(elements, display) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style['display'] = display;
+    }
+
+    return elements;
+}
+
+// Payment form delievery fields
+let toAddress = 'Доставка до адрес';
+let toRestaurant = 'В наш ресторант';
+
+let addressFields = document.getElementsByClassName('address-none');
+
+let checkboxElements = document.getElementsByClassName('user-address');
+
+for (let i = 0; i < checkboxElements.length; i++) {
+    let element = checkboxElements[i];
+
+    element.addEventListener('click',
+        function (event) {
+            for (var y = 0; y < addressFields.length; y++) {
+
+                if (element.checked && element.value === toAddress) {
+                    addressFields[y].style['display'] = '';
+                } else if (element.checked && element.value === toRestaurant) {
+                    addressFields[y].style['display'] = 'none';
+                }
+            }
         }
-    });
+    );
+}
