8d56cd40e6ffccdbe05cd912b19021e1ec8590e0
Hristoki <54570662+Hristoki@users.noreply.github.com>
2022-04-03 20:44:58 +0300

    Changed table style and cart item logic

--- Web/PastaWorld.Web/Controllers/BaseController.cs
+++ Web/PastaWorld.Web/Controllers/BaseController.cs
@@ -1,30 +1,17 @@
 ﻿namespace PastaWorld.Web.Controllers
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Filters;
     using Newtonsoft.Json;
     using PastaWorld.Web.ViewModels.Cart;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Text;
 
     public class BaseController : Controller
     {
-        //public override void OnActionExecuting(ActionExecutingContext filterContext)
-        //{
-        //    var cart = new List<CartItemViewModel>();
-        //    var cartIsNotEmpty = this.HttpContext.Session.TryGetValue("cart", out byte[] cartContentAsByteArray);
-        //    if (cartIsNotEmpty)
-        //    {
-        //    var reader = new StreamReader(new MemoryStream(cartContentAsByteArray), Encoding.Default);
-        //    cart = new Newtonsoft.Json
-        //            .JsonSerializer()
-        //            .Deserialize<List<CartItemViewModel>>(new JsonTextReader(reader));
-
-        //    }
-        //    this.ViewBag.CartItems = cart.Count;
-        //}
-
         public override void OnActionExecuted(ActionExecutedContext context)
         {
             var cart = new List<CartItemViewModel>();
@@ -37,7 +24,7 @@
                         .Deserialize<List<CartItemViewModel>>(new JsonTextReader(reader));
             }
 
-            this.ViewBag.CartItems = cart.Count;
+            this.ViewBag.CartItems = cart.Sum(x => x.Quantity);
         }
     }
 }
