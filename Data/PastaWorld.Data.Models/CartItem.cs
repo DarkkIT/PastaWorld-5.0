1545500633138bf174dc98b89df50e50f5204d3d
EmilVuchev <emo_vuchev_90@abv.bg>
2022-05-20 22:34:19 +0300

    Adds Order object in CartItem

--- Data/PastaWorld.Data.Models/CartItem.cs
+++ Data/PastaWorld.Data.Models/CartItem.cs
@@ -4,8 +4,12 @@
 
     public class CartItem : BaseDeletableModel<int>
     {
+        public int Quantity { get; set; }
+
         public Meal Meal { get; set; }
 
-        public int Quantity { get; set; }
+        public int OrderId { get; set; }
+
+        public Order Order { get; set; }
     }
 }
